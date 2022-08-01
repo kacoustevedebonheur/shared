c2249c76d14846dc2b81787eddca47c190bab34a
Steve de bonheur KACOU <107809265+kacoustevedebonheur@users.noreply.github.com>
2022-08-01 18:40:28 +0200

    Create move-up.ts

---
+++ move-up.ts
@@ -0,0 +1,27 @@
+    /**
+     * reorder the list by putting the selected elements at the beginning of the list
+     * @param list 
+     * @param selected 
+     * @returns 
+     */
+    moveSelectedOnTop(list: any[], selected: Array<number|string>) {
+        if(!selected?.length) {
+            return list;
+        }
+
+        if(!list?.length) {
+            return [];
+        }
+
+        const listCopy = list.map(ct => ct);
+
+        // Retirer tous les éléments selectionnés de la liste 
+        list = list.filter(ct =>  !selected?.includes(ct.id || ct.code));
+    
+        // Les inserer en tête de list 
+        const  selectedElements =  listCopy.filter(ct =>  selected?.includes(ct.id || ct.code));
+
+        list.unshift(...selectedElements || []);
+
+        return list;
+    }
